8d2820cb5828d3bc1b57124caf2c649b5e2f1a84
sebastienDeWilde <sebastien.dewilde@busi.eu>
2019-12-04 16:49:48 +0100

    correction xsd and add not used

--- src/main/java/be/iso/viapass/util/ConstantsISO.java
+++ src/main/java/be/iso/viapass/util/ConstantsISO.java
@@ -14,24 +14,25 @@ public final class ConstantsISO {
 
         /*************** All path of Common xsd file in the project****************/
         public static final String ASN1_XSD_PATH = "src/main/resources/xsdFiles/asn1.xsd";
+        public static final String SCHEMA_VALIDATION_XSD_ISO_12855_XSD_PATH = "src/main/resources/xsdFiles/SchemaValidationXSDISO12855.xsd";
         public static final String COMMON_XSD_PATH = "src/main/resources/xsdFiles/Common.xsd";
-        public static final String COMMON_APCI_XSD_PATH = "src/main/resources/xsdFiles/CommonAPCI.xsd";
+        public static final String COMMON_APCI_ADU_TYPE_XSD_PATH = "src/main/resources/xsdFiles/CommonAPCI_ADUType.xsd";
 
         /*************** All path of response xsd file in the project****************/
-        public static final String RESPONSE_ABNORMAL_OBE_XSD_PATH = "src/main/resources/xsdFiles/Response_Abnormal_OBE.xsd";
-        public static final String RESPONSE_PROVIDE_DAILY_STATEMENT_XSD_PATH = "src/main/resources/xsdFiles/Response_Provide_Daily_Statement.xsd";
-        public static final String RESPONSE_WLBL_XSD_PATH = "src/main/resources/xsdFiles/Response_WLBL.xsd";
-        public static final String RESPONSE_ADDRESS_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_Address_Data.xsd";
-        public static final String RESPONSE_VEHICLE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_Vehicle_Data.xsd";
-        public static final String RESPONSE_OBU_COMPLIANCE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_OBU_Compliance_Data.xsd";
+//        public static final String RESPONSE_ABNORMAL_OBE_XSD_PATH = "src/main/resources/xsdFiles/Response_Abnormal_OBE.xsd";
+//        public static final String RESPONSE_PROVIDE_DAILY_STATEMENT_XSD_PATH = "src/main/resources/xsdFiles/Response_Provide_Daily_Statement.xsd";
+//        public static final String RESPONSE_WLBL_XSD_PATH = "src/main/resources/xsdFiles/Response_WLBL.xsd";
+//        public static final String RESPONSE_ADDRESS_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_Address_Data.xsd";
+//        public static final String RESPONSE_VEHICLE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_Vehicle_Data.xsd";
+//        public static final String RESPONSE_OBU_COMPLIANCE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Response_OBU_Compliance_Data.xsd";
 
         /*************** All path of request xsd file in the project****************/
-        public static final String REQUEST_ANORMAL_OBE_XSD_PATH = "src/main/resources/xsdFiles/Request_Abnormal_OBE.xsd";
-        public static final String REQUEST_PROVIDE_DAILY_STATEMENT_XSD_PATH = "src/main/resources/xsdFiles/Request_Provide_Daily_Statement.xsd";
-        public static final String REQUEST_WLBL_XSD_PATH = "src/main/resources/xsdFiles/Request_WLBL.xsd";
-        public static final String REQUEST_ADDRESS_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_Address_Data.xsd";
-        public static final String REQUEST_VEHICLE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_Vehicle_Data.xsd";
-        public static final String REQUEST_OBU_COMPLIANCE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_OBU_Compliance_Data.xsd";
+//        public static final String REQUEST_ANORMAL_OBE_XSD_PATH = "src/main/resources/xsdFiles/Request_Abnormal_OBE.xsd";
+//        public static final String REQUEST_PROVIDE_DAILY_STATEMENT_XSD_PATH = "src/main/resources/xsdFiles/Request_Provide_Daily_Statement.xsd";
+//        public static final String REQUEST_WLBL_XSD_PATH = "src/main/resources/xsdFiles/Request_WLBL.xsd";
+//        public static final String REQUEST_ADDRESS_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_Address_Data.xsd";
+//        public static final String REQUEST_VEHICLE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_Vehicle_Data.xsd";
+//        public static final String REQUEST_OBU_COMPLIANCE_DATA_XSD_PATH = "src/main/resources/xsdFiles/Request_OBU_Compliance_Data.xsd";
 
 
 
